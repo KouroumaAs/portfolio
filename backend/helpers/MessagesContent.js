92cdb3e9b2fcc8e22cfee65c4ede3f687485c5b0
KouroumaAs <boubasid2000@yahoo.fr>
2023-11-24 18:10:47 +0000

    Contact Page

--- backend/helpers/MessagesContent.js
+++
@@ -1,8 +0,0 @@
-function MessagesVisitor(data) {
-    var html = `<div>
-                   <h3> ${data.nom}</h3>
-                   <p style="margin-top:10px;font-size:14px;font-weigth:300">${data.message}</p>
-                </div>`;
-   return html;             
-} 
-module.exports = {MessagesVisitor}
